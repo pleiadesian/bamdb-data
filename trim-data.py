a79a941f28e115ed574df561b01145cffe2d8170
pleiadesian <wzl574402791@outlook.com>
2019-07-29 11:41:42 +0800

    finish scratch

--- trim-data.py
+++ trim-data.py
@@ -42,26 +42,39 @@ with open("./a.sql") as f:
             item_compile = re.compile(r'[(](.*?)[)]', re.S)
             items = re.findall(item_compile, line)
             for item_line in items:
-                # problems in info columns
                 columns = item_line.split(',')
+                if columns[-1] != '0' or columns[5][1:-1] != "tags":
+                    continue
                 item_data = dict()
                 item_data["itemId"] = columns[0]
                 item_data["title"] = columns[4][1:-1]
                 item_data["imgurl"] = columns[2][1:-1]
                 item_data["type"] = columns[3][1:-1]
-                item_data["info"] = dict()
-                info = json.loads(columns[6][-1:1])
-                item_data["info"] = info
+                # item_data["info"] = dict()
+                # info = json.loads(columns[6][-1:1])
+                # item_data["info"] = info
                 item_data["score_details"] = dict()
-                for i in range(1, 10):
-                    item_data["score_details"][str(i)] = json.loads(columns[7])[str(i)]
-                item_data["score_details"]["total"] = columns[7]["total"]
-                item_data["avgScore"] = columns[8][1:-1]
-                item_data["wishes"] = columns[9]
-                item_data["done"] = columns[10]
-                item_data["doings"] = columns[11]
-                item_data["on_hold"] = columns[12]
-                item_data["dropped"] = columns[13]
+                # for i in range(1, 10):
+                #     item_data["score_details"][str(i)] = json.loads(columns[7])[str(i)]
+                temp = columns[-19].split(':')
+                item_data["score_details"]["total"] = columns[-19].split(':')[1][3:-2]
+                item_data["score_details"]["10"] = columns[-18].split(':')[1][3:-2]
+                item_data["score_details"]["9"] = columns[-17].split(':')[1][3:-2]
+                item_data["score_details"]["8"] = columns[-16].split(':')[1][3:-2]
+                item_data["score_details"]["7"] = columns[-15].split(':')[1][3:-2]
+                item_data["score_details"]["6"] = columns[-14].split(':')[1][3:-2]
+                item_data["score_details"]["5"] = columns[-13].split(':')[1][3:-2]
+                item_data["score_details"]["4"] = columns[-12].split(':')[1][3:-2]
+                item_data["score_details"]["3"] = columns[-11].split(':')[1][3:-2]
+                item_data["score_details"]["2"] = columns[-10].split(':')[1][3:-2]
+                item_data["score_details"]["1"] = columns[-9].split(':')[1][3:-4]
+
+                item_data["avgScore"] = columns[-8][1:-1]
+                item_data["wishes"] = columns[-7]
+                item_data["done"] = columns[-6]
+                item_data["doings"] = columns[-5]
+                item_data["on_hold"] = columns[-4]
+                item_data["dropped"] = columns[-3]
                 item_set.append(item_data)
     json.dump(relation_set, open('./bamgumi-relation.json', 'w'), ensure_ascii=False, indent=1)
     json.dump(tag_set, open('./bamgumi-tag.json', 'w'), ensure_ascii=False, indent=1)
