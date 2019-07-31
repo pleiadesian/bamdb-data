652ed1e9a28d3ff4eeb9644dd5ec6d34c8c4e8f9
root <blasi@vip.qq.com>
2019-07-31 14:03:43 +0800

    staff8

--- staff-data.py
+++ staff-data.py
@@ -10,6 +10,11 @@ output = []
 data = json.load(open("./bangumi-item.json", 'r'))
 i = 50186
 for item_in in data:
+    if i < 80000:
+        i += 1
+        continue
+    if i == 90000:
+        break
     h = httplib2.Http(".cache")
     resp, content = h.request("http://mirror.api.bgm.rin.cat/subject/"+str(item_in["itemId"]) +
                               "?responseGroup=medium", "GET")
