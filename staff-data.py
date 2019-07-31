a42ed5bd00ec4ea823501cc6ff1806467a48818b
pleiadesian <wzl574402791@outlook.com>
2019-07-31 10:20:24 +0800

    scrapy cluster

--- staff-data.py
+++ staff-data.py
@@ -11,16 +11,21 @@ data = json.load(open("./bangumi-item.json", 'r'))
 i = 50186
 for item_in in data:
     h = httplib2.Http(".cache")
-    resp, content = h.request("https://mirror.api.bgm.rin.cat/subject/"+str(item_in["itemId"]) +
+    resp, content = h.request("http://mirror.api.bgm.rin.cat/subject/"+str(item_in["itemId"]) +
                               "?responseGroup=medium", "GET")
     item_info = dict()
+    staff = "暂无"
     if resp.status == 200:
         item_info = json.loads(content)
         item_info["itemId"] = item_in["itemId"]
-        item_info["staff"] = item_info["staff"][0]["name_cn"]
-        if item_info["staff"] == "" or item_info["staff"] is None:
-            item_info["staff"] = item_info["staff"][0]["name"]
-    sql_line = "update bamdb.item set main_author=" + item_info["staff"] + " where id=" + str(i) + "\n"
+        if item_info["staff"] is None:
+            staff = "暂无"
+        else:
+            staff = item_info["staff"][0]["name_cn"]
+            if staff == "" or staff is None:
+                staff = item_info["staff"][0]["name"]
+    sql_line = "update bamdb.item set main_author=" + staff + " where id=" + str(i) + "\n"
     staffs.append(sql_line)
     i += 1
+    print(i)
 open('./bangumi-staff.sql', 'w').writelines(staffs)
